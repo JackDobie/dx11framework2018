b3a704bf18af18c561857d794f1949fc4d6ffe27
Jack Dobie <57402187+JackDobie@users.noreply.github.com>
2020-10-19 10:54:31 +0100

    added depth buffer! objects look normal now

--- Application.cpp
+++ Application.cpp
@@ -372,7 +372,7 @@ HRESULT Application::InitDevice()
     if (FAILED(hr))
         return hr;
 
-    _pImmediateContext->OMSetRenderTargets(1, &_pRenderTargetView, nullptr);
+    
 
     // Setup the viewport
     D3D11_VIEWPORT vp;
@@ -413,6 +413,28 @@ HRESULT Application::InitDevice()
     if (FAILED(hr))
         return hr;
 
+    D3D11_TEXTURE2D_DESC depthStencilDesc;
+
+    depthStencilDesc.Width              = _WindowWidth;
+    depthStencilDesc.Height             = _WindowHeight;
+    depthStencilDesc.MipLevels          = 1;
+    depthStencilDesc.ArraySize          = 1;
+    depthStencilDesc.Format             = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    depthStencilDesc.SampleDesc.Count   = 1;
+    depthStencilDesc.SampleDesc.Quality = 0;
+    depthStencilDesc.Usage              = D3D11_USAGE_DEFAULT;
+    depthStencilDesc.BindFlags          = D3D11_BIND_DEPTH_STENCIL;
+    depthStencilDesc.CPUAccessFlags     = 0;
+    depthStencilDesc.MiscFlags          = 0;
+
+    _pd3dDevice->CreateTexture2D(&depthStencilDesc, nullptr, &_depthStencilBuffer);//HERE
+    _pd3dDevice->CreateDepthStencilView(_depthStencilBuffer, nullptr, &_depthStencilView);
+
+    if (FAILED(hr))
+        return hr;
+
+    _pImmediateContext->OMSetRenderTargets(1, &_pRenderTargetView, _depthStencilView);
+
     return S_OK;
 }
 
@@ -430,6 +452,8 @@ void Application::Cleanup()
     if (_pSwapChain) _pSwapChain->Release();
     if (_pImmediateContext) _pImmediateContext->Release();
     if (_pd3dDevice) _pd3dDevice->Release();
+    if (_depthStencilView) _depthStencilView->Release();
+    if (_depthStencilBuffer) _depthStencilBuffer->Release();
 }
 
 void Application::Update()
@@ -468,6 +492,8 @@ void Application::Draw()
     float ClearColor[4] = {0.0f, 0.125f, 0.3f, 1.0f}; // red,green,blue,alpha
     _pImmediateContext->ClearRenderTargetView(_pRenderTargetView, ClearColor);
 
+    _pImmediateContext->ClearDepthStencilView(_depthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
 	XMMATRIX world = XMLoadFloat4x4(&_world);
 	XMMATRIX view = XMLoadFloat4x4(&_view);
 	XMMATRIX projection = XMLoadFloat4x4(&_projection);
@@ -490,6 +516,7 @@ void Application::Draw()
 	_pImmediateContext->PSSetShader(_pPixelShader, nullptr, 0);
 	_pImmediateContext->DrawIndexed(36, 0, 0);        
 
+    //draws second cube
     world = XMLoadFloat4x4(&_world2);
     cb.mWorld = XMMatrixTranspose(world);
     _pImmediateContext->UpdateSubresource(_pConstantBuffer, 0, nullptr, &cb, 0, 0);
